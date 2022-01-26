0b745d759fc2d63b7f62ad9da2a4af2fbb96f4b4
sathishKumar Rajendran <sathishKumar.rajendran.net@gmail.com>
2022-01-26 17:37:05 +0000

    test demo commit

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace DockerSupport
         {
             services.AddRazorPages();
 
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
